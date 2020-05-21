12d40455f9bbf97027d939cbf7a87ccc7596b176
Kyle Negley <kpnegley@gmail.com>
2020-05-21 14:31:14 -0400

    got sorted list to work

--- wordcount.py
+++ wordcount.py
@@ -25,29 +25,57 @@ should return a dictionary with words as keys, and their counts as values.
 
 # Your name, plus anyone who helped you with this assignment
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Kyle"
 
 import sys
 
 
 def create_word_dict(filename):
     """Returns a word/count dict for the given file."""
-    # Your code here
-    return
+
+    word_list = []
+    word_set = set()
+    word_dict = {}
+    with open("books/"+filename) as f:
+
+        for line in f:
+            word_list = [*word_list, *(line.split())]
+
+    word_set = set(word_list)
+
+    for word in word_set:
+        word_dict[word] = 0
+
+    for word in word_list:
+        if word in word_dict:
+            word_dict[word] += 1
+
+    return word_dict
+
+
+# create_word_dict("alice.txt")
 
 
 def print_words(filename):
     """Prints one per line '<word> : <count>', sorted
     by word for the given file.
     """
-    # Your code here
-    return
+
+    print(create_word_dict(filename))
+
+    return create_word_dict(filename)
 
 
 def print_top(filename):
     """Prints the top count listing for the given file."""
-    # Your code here
-    return
+    print(dict(sorted(create_word_dict(filename).items(),
+                      key=lambda x: x[1], reverse=True)))
+
+    return dict(sorted(create_word_dict(filename).items(),
+                       key=lambda x: x[1], reverse=True))
+
+
+# print_top("alice.txt")
 
 
 # This basic command line argument parsing code is provided and calls
